571838f51239394d52f38fe9f5dc379c0b119e1b
michael <tianya0yingzi@163.com>
2014-12-21 22:20:06 +0800

    add allwords

--- lua/iterator.lua
+++ lua/iterator.lua
@@ -17,8 +17,49 @@ function ipairs(...)
     return mult_ipairs_it, t, 0
 end
 
-local t1 = {'a','b','c','d','e'}
-local t2 = {'A','B','C','D','E','F'}
-for k,v1,v2 in ipairs(t1,t2) do
-    print(k,v1,v2)
+--local t1 = {'a','b','c','d','e'}
+--local t2 = {'A','B','C','D','E','F'}
+---for k,v1,v2 in ipairs(t1,t2) do
+---    print(k,v1,v2)
+---end
+
+function values (t)
+    local i = 0
+    return   function () i = i+1; return t[i]
+    end
 end
+
+function vm ()
+    local t1 = {'a','b','c','d','e'}
+    local t = {10,20,30}
+    iter = values(t1)
+    while true do
+        local element = iter()
+        if element == nil then break; end
+        print(element)
+    end
+
+    for element in values(t) do
+        print(element);
+    end
+end
+
+function allwords ()
+    local line = io.read()
+    local pos = 1
+    return function ()
+        while line do
+            local s, e = string.find(line, "%w+", pos)
+            if s then
+                pos = e + 1
+                return string.sub(line, s, e)
+            else line = io.read()
+                pos = 1
+            end
+        end
+    end
+end
+for i in allwords() do
+    print(i)
+end
+
